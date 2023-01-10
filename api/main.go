b04c12e0731d1de99b053353062603a8e070dd8e
Saku Kaarakainen <sakuba91@hotmail.com>
2023-01-10 10:35:00 +0200

    Setup redis

--- api/main.go
+++ api/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9" // uses redis7
+
+	"github.com/saku-kaarakainen/personality-test-app/api/routes"
 )
 
-func main() {
-	server := gin.Default()
+var ctx = context.Background()
 
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "OK",
-		})
+func setupRedis() {
+	rdb := redis.NewClient(&redis.Options{
+		// TODO: Add username and password, also .env
+		Addr:     "personality-test-db:6379",
+		Password: "", // no password set
+		DB:       0,  // use default db
 	})
 
+	pong, err := rdb.Ping(ctx).Result()
+	fmt.Println("Redis Ping:")
+	fmt.Println(pong, err)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setupServer() {
+	server := gin.Default()
+	server.GET("/questions", routes.Get_questions)
 	server.Run(":8080")
+}
+
+func main() {
+	setupRedis()
+	setupServer()
 }
