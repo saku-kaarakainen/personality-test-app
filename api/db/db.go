073b1bba06690e0affa186f134e98fee86510c62
Saku Kaarakainen <sakuba91@hotmail.com>
2023-01-10 14:39:49 +0200

    commit

---
+++ api/db/db.go
@@ -0,0 +1,80 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	goredis "github.com/go-redis/redis/v8" // uses redis7
+	"github.com/gomodule/redigo/redis"
+	"github.com/nitishm/go-rejson/v4"
+)
+
+type Test struct {
+	Foo     string `json:"foo,omitempty"`
+	SomeKey string `json:"some_key,omitempty"`
+}
+
+var (
+	ctx = context.Background()
+	cli = goredis.NewClient(&goredis.Options{
+		// TODO: Add username and password, also .env
+		Addr:     "personality-test-db:6379",
+		Password: "", // no password set
+		DB:       0,  // use default db
+	})
+	rh = rejson.NewReJSONHandler()
+)
+
+func init() {
+	ping()
+
+	rh.SetGoRedisClient(cli)
+	populate(rh)
+}
+
+func ping() {
+	pong, err := cli.Ping(ctx).Result()
+	fmt.Println("Redis Ping:")
+	fmt.Println(pong, err)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func populate(rh *rejson.Handler) {
+	fmt.Println("starting to populate")
+	test := Test{
+		Foo:     "hardcoded1",
+		SomeKey: "hardcoded2",
+	}
+
+	res, err := rh.JSONSet("test", ".", test)
+	if err != nil {
+		log.Fatalf("Failed to JSONSet")
+		return
+	}
+
+	fmt.Println("JSON with key 'test' set!")
+
+	if res.(string) == "OK" {
+		fmt.Printf("Success: %s\n", res)
+	} else {
+		fmt.Println("Failed to Set: ")
+	}
+
+	testJSON, err := redis.Bytes(rh.JSONGet("test", "."))
+	if err != nil {
+		fmt.Println("Failed to JSONGet")
+		panic("Failed to JSONGet")
+	}
+
+	fmt.Println(("found json: " + string(testJSON)))
+}
+
+func LoadModule() {
+	// makes sure init is called.
+	// init is lazy-load so it will be called
+	// when the module is referred first time.
+}
--- api/main.go
+++ api/main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis/v9" // uses redis7
 
+	"github.com/saku-kaarakainen/personality-test-app/api/db"
 	"github.com/saku-kaarakainen/personality-test-app/api/routes"
 )
-
-var ctx = context.Background()
-
-func setupRedis() {
-	rdb := redis.NewClient(&redis.Options{
-		// TODO: Add username and password, also .env
-		Addr:     "personality-test-db:6379",
-		Password: "", // no password set
-		DB:       0,  // use default db
-	})
-
-	pong, err := rdb.Ping(ctx).Result()
-	fmt.Println("Redis Ping:")
-	fmt.Println(pong, err)
-
-	if err != nil {
-		panic(err)
-	}
-}
 
 func setupServer() {
 	server := gin.Default()
@@ -36,6 +14,6 @@ func setupServer() {
 }
 
 func main() {
-	setupRedis()
+	db.LoadModule()
 	setupServer()
 }
