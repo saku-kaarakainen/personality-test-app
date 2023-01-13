187e2d49b27812531318f6a0b72a3a8fa071ce1c
Saku Kaarakainen <sakuba91@hotmail.com>
2023-01-13 11:46:40 +0200

    Call backend from frontend, although it's still blocked by cors

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,25 @@
+import axios from 'axios';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const baseURL = "http://localhost:8080"
+
 function App() {
+  const [questions, setQuestions] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios.get(`${baseURL}/questions`)
+      console.log("Got data succesfully:", results)
+    }
+
+    fetchData()
+      .catch((error) => {
+        console.error("Error in in fetching data:", error)
+      })
+   })
+
   return (
     <div className="App">
       <header className="App-header">
