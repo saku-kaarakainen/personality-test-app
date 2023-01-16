2291a14dd8c3566bb9217877e49be7d89a125f90
Saku Kaarakainen <sakuba91@hotmail.com>
2023-01-16 14:21:08 +0200

    feat(app): navigation buttons (#16)
    added navigation buttons, without logic, for questions, previous and next

--- app/src/pages/Questions.tsx
+++ app/src/pages/Questions.tsx
@@ -1,39 +1,71 @@
 import React from 'react'
 import axios from 'axios'
+import { Button, Row, Col } from 'antd'
 import config from './../config.json'
 import Question, { QuestionSet } from '../components/Question'
+import './Questions.css'
 
 const Questions: React.FC = () => {
   const [questions, setQuestions] = React.useState(new Array<QuestionSet>())
-  const [currentStep] = React.useState(1)
-  const [maxSteps, setMaxSteps] = React.useState(1)
+  const [currentStep, setCurrentStep] = React.useState(0)
 
   React.useEffect(() => {
     const fetchData = async (): Promise<void> => {
       const results = await axios.get(`${config.apiBaseUrl}/questions`)
+
       setQuestions(results.data)
-      setMaxSteps(questions.length)
     }
 
     fetchData().catch((error) => { console.error('Error in in fetching data:', error) })
-  }, [])
-
-  /*
-    TODO:
-     - next question
-     - previous question
-     - tests
-  */
+  }, [questions])
 
   if (questions.length === 0) {
     // TODO: Show loading spinner, when the results are not available
     return <></>
   }
 
-  return <Question
-    questions={questions}
-    currentStep={currentStep}
-    maxSteps={maxSteps} />
+  const onPrevClick = (): void => {
+    // if index is 1 or 2, set to 1
+    if (currentStep < 1) {
+      console.log('set first page')
+
+      // First step, reset to default
+      setCurrentStep(0)
+      return
+    }
+
+    console.log('set prev page')
+    setCurrentStep(currentStep - 1)
+  }
+
+  const onNextClick = (): void => {
+    const lastIndex = questions.length - 1
+
+    if (currentStep < lastIndex) {
+      setCurrentStep(currentStep + 1)
+    } else {
+      console.log('send results')
+    }
+  }
+
+  return (<>
+    <Row>
+      <Col span={24} className='col-question'>
+        <Question
+          questions={questions}
+          currentStep={currentStep}
+          maxSteps={questions.length} />
+      </Col>
+    </Row>
+    <Row>
+      <Col span={12} className='col-prev' onClick={onPrevClick}>
+        <Button>{currentStep === 0 ? 'Home page' : 'Previous'}</Button>
+      </Col>
+      <Col span={12} className='col-next' onClick={onNextClick}>
+        <Button type="primary">{currentStep === questions.length - 1 ? 'See results' : 'Next'}</Button>
+      </Col>
+    </Row>
+  </>)
 }
 
 export default Questions
