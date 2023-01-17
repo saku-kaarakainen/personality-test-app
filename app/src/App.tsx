e4157a4342748d271936cdd8756e4067cdaff792
Saku Kaarakainen <sakuba91@hotmail.com>
2023-01-17 16:26:29 +0200

    created results page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,32 +10,27 @@ import './App.css'
 const { Header, Footer, Content } = Layout
 
 const App: React.FC = () => {
-  return (
-    <>
-      <BrowserRouter>
-        {/* Only one layout, that is mobile-friendly */}
-        <Layout>
-          <Header id="app-header">
-            <h1>
-              <Link id="title-link" to="/">Personality Test App</Link>
-            </h1>
-          </Header>
-          <Content id="app-content">
-            <h2>Are you an introvert or an extrovert?</h2>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="questions" element={<Questions />} />
-              <Route path="result" element={<Result />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Content>
-          <Footer id="app-footer">
+  return <BrowserRouter>
+    {/* Only one layout, that is mobile-friendly */}
+    <Layout>
+      <Header id="app-header">
+        <h1>
+          <Link id="title-link" to="/">Personality Test App</Link>
+        </h1>
+      </Header>
+      <Content id="app-content">
+        <h2>Are you an introvert or an extrovert?</h2>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="questions" element={<Questions />} />
+          <Route path="result" element={<Result />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Content>
+      <Footer id="app-footer">
 
-          </Footer>
-        </Layout>
-      </BrowserRouter>
-    </>
-  )
+      </Footer>
+    </Layout>
+  </BrowserRouter>
 }
-
 export default App
